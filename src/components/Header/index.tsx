332b81700e0f3db39fea8a52c54c0190b43f4fcc
nanashi7c <nanashi7c7c@gmail.com>
2026-04-20 22:25:55 +0900

    fix: PR: #15 レビュー指摘対応 - LinkとButtonの不正なネスト構造を解消
    - LinkとButtonのネストを解消
    - Linkにボタンスタイルを適用
    - ボタン用のスタイルをstyles.module.cssに追加

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,24 +6,24 @@ import Button from "../Button";
 import Link from "next/link";
 
 export const Header = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
   return (
     <header className={styles.header}>
       {isLoggedIn ? (
         <div className={styles.buttonWrapper}>
-          <Link href="/articles/new">
-            <Button label="新規作成" variant="secondary" />
+          <Link href="/articles/new" className={`${styles.button}  ${styles.secondaryButton} ${styles.mediumButton}`}>
+            新規作成
           </Link>
           <Button label="ログアウト" onClick={() => setIsLoggedIn(false)} />
         </div>
       ) : (
         <div className={styles.buttonWrapper}>
-          <Link href="/login">
-            <Button label="ログイン" variant="secondary" />
+          <Link href="/login" className={`${styles.button}  ${styles.secondaryButton} ${styles.mediumButton}`}>
+            ログイン
           </Link>
-          <Link href="/signup">
-            <Button label="新規登録" />
+          <Link href="/signup" className={`${styles.button}  ${styles.primaryButton} ${styles.mediumButton}`}>
+            新規登録
           </Link>
         </div>
       )}
