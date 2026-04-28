44e3ce9a7a10b7019e5aa14746be6dfe34a334f1
Ichikawa <ricainais1@gmail.com>
2026-04-28 10:54:13 +0900

    TextBoxのエラー表示修正

--- src/components/TextBox/index.tsx
+++ src/components/TextBox/index.tsx
@@ -4,7 +4,7 @@ import { TextBoxProps } from "./type";
 const TextBox = ({ placeholder = "本文を入力してください", error, ...props }: TextBoxProps) => {
   return (
     <div>
-      <textarea placeholder={placeholder} className={`${styles.textBox} ${error ? styles.error : ""}`} {...props} />
+      <textarea placeholder={placeholder} className={`${styles.textBox} ${error && styles.error}`} {...props} />
       {error && <p className={styles.errorMessage}>{error}</p>}
     </div>
   );
