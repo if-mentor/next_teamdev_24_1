7b0b0cf6f6a9262decf4910960c73ceb50168869
Ichikawa <ricainais1@gmail.com>
2026-05-02 23:49:11 +0900

    [FE] 記事投稿画面 (#36)
    * 記事投稿画面の実装
    
    * TextBoxコンポーネント作成と記事投稿画面の修正
    
    * 投稿画面の修正
    
    * TextBoxレビュー後の修正対応 + articles/newのUI修正
    
    * コメントアウトの箇所を削除
    
    * TextBoxのエラー表示修正
    
    * gitignoreを戻す
    
    * ImageUploaderPreviewコンポーネント挿入とデザイン修正
    
    * TextBoxのデザイン修正
    
    * 不要なプレースホルダ.gitignore削除
    
    * Headerの削除

---
+++ src/app/articles/new/page.tsx
@@ -0,0 +1,33 @@
+import Button from "@/components/Button";
+import Input from "@/components/Input";
+import SelectBox from "@/components/SelectBox";
+import styles from "./styles.module.css";
+import TextBox from "@/components/TextBox";
+import ImageUploaderPreview from "@/components/ImageUploaderPreview";
+
+export default function ArticleNewPage() {
+  return (
+    <div>
+      <main className={styles.wrapper}>
+        <div className={styles.inputWrapper}>
+          <Input type="text" name="title" placeholder="タイトルを入力" size="large" />
+        </div>
+
+        <div className={styles.imageUpload}>
+          <ImageUploaderPreview />
+        </div>
+
+        <SelectBox
+          options={["日常", "仕事", "勉強", "美容", "趣味", "購入品", "健康", "その他"]}
+          placeholder="選択してください"
+        />
+
+        <TextBox />
+
+        <div className={styles.buttonWrapper}>
+          <Button label="投稿" variant="success" size="medium" />
+        </div>
+      </main>
+    </div>
+  );
+}
