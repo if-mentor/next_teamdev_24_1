a9f6b7a8b5cf3369559b8cfd514a7fedb8283f85
Rio-Sasaki <sorario10140826@gmail.com>
2026-05-23 21:51:14 +0900

    SelectBox value属性 追加

--- src/components/SelectBox/index.tsx
+++ src/components/SelectBox/index.tsx
@@ -7,11 +7,17 @@ const SelectBox = ({ label, options, placeholder, ...selectProps }: SelectProps)
       <label className={styles.label}>{label}</label>
       <select className={styles.select} {...selectProps}>
         <option value="">{placeholder}</option>
-        {options.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
+        {options.map((option) =>
+          typeof option === "string" ? (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ) : (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ),
+        )}
       </select>
     </>
   );
